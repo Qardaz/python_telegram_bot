9c659d9af46f3e18261023aa6ef228822ab04585
Kartyshov Dmitry <kartyshov.dmitry@yandex.ru>
2024-05-03 21:11:20 +0300

    Объединил несколько функций в одну и удалил лишние файлы.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import telebot
-from low import low_func
-from high import high_func
-from custom import custom_func
-from hot import hot_func
+from all_in_one import all_in_one_func
 
 
 token = '6855239878:AAEnHljJy9MjG9vfqqi8hsjnTksnEv0Bd98'
@@ -14,48 +11,50 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Привет :)')
 
 
-@bot.message_handler(commands=['low', 'high', 'custom', 'hot'])
+@bot.message_handler(commands=['low', 'high', 'hot', 'custom'])
 def main(message):
     if message.text.lower() == '/low':
         bot.send_message(message.chat.id, 'Сколько товаров вывести?')
 
         @bot.message_handler(content_types=['text'])
         def how_many(message):
-            how_many = message.text
-            bot.send_message(message.chat.id, f'{low_func(how_many)}')
+            user_input = message.text
+            bot.send_message(message.chat.id, f'{all_in_one_func(user_input)}')
 
     elif message.text.lower() == '/high':
         bot.send_message(message.chat.id, 'Сколько товаров вывести?')
 
         @bot.message_handler(content_types=['text'])
         def how_many(message):
-            how_many = message.text
-            bot.send_message(message.chat.id, f'{high_func(how_many)}')
+            user_input = message.text
+            bot.send_message(message.chat.id, f'{all_in_one_func(how_many=user_input, reverse=True)}')
 
     elif message.text.lower() == '/hot':
         bot.send_message(message.chat.id, 'Сколько товаров вывести?')
 
         @bot.message_handler(content_types=['text'])
         def how_many(message):
-            how_many = message.text
-            bot.send_message(message.chat.id, f'{hot_func(how_many)}')
+            user_input = message.text
+            bot.send_message(message.chat.id, all_in_one_func(how_many=user_input, sorted_by='orders_quantity'))
 
-    # elif message.text.lower() == '/custom':
-    #     @bot.message_handler(content_types=['text'])
-    #     def how_many(message):
-    #         bot.send_message(message.chat.id, 'Сколько товаров вывести?')
-    #         how_many = message.text
-    #
-    #     @bot.message_handler(content_types=['text'])
-    #     def upper_limit(message):
-    #         bot.send_message(message.chat.id, 'Введите нижнюю границу.')
-    #         lower_limit = message.text
-    #
-    #     @bot.message_handler(content_types=['text'])
-    #     def lower_limit(message):
-    #         bot.send_message(message.chat.id, 'Введите верхнюю границу.')
-    #         upper_limit = message.text
-    #         bot.send_message(message.chat.id, f'{custom_func(how_many, lower_limit, upper_limit)}')
+    elif message.text.lower() == '/custom':
+        bot.send_message(
+            message.chat.id,
+            'Напишите через пробел, сколько товаров вывести, нижнюю границу и верхнюю границу цен.'
+        )
+
+        @bot.message_handler(content_types=['text'])
+        def how_many(message):
+            user_input = message.text.split()
+
+            how_many = int(user_input[0])
+            low_lim = int(user_input[1])
+            high_lim = int(user_input[2])
+
+            bot.send_message(
+                message.chat.id,
+                f'{all_in_one_func(how_many=how_many, low_lim=low_lim, high_lim=high_lim)}'
+            )
 
 
 bot.polling(none_stop=True)
