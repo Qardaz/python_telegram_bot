9c286bd38be836b357dbaaee67f3ffc8206c4945
Kartyshov Dmitry <kartyshov.dmitry@yandex.ru>
2024-05-13 14:45:06 +0300

    Реализовал последовательный вызов хэндлеров, команду /hello_world, реагирование бота на текст "привет" и переименовал functions.py в sort_goods_list_functions.py

--- commands.py
+++ commands.py
@@ -1,6 +1,8 @@
 from telebot.types import BotCommand
 
 default_commands = [
+    # BotCommand('hello-world', 'привет, мир'),
+    BotCommand('hello_world', 'привет, мир'),
     BotCommand('start', 'начало работы'),
     BotCommand('help', 'помощь'),
     BotCommand('low', 'сортировка товаров по возрастанию цены'),
--- main.py
+++ main.py
@@ -1,44 +1,75 @@
 import telebot
+
 from config import token
-from functions import command_func
+from sort_goods_list_function import command_func
 from commands import default_commands
 import messages
+
 
 bot = telebot.TeleBot(token)
 
 
+# @bot.message_handler(commands=['hello-world'])
+
+@bot.message_handler(commands=['hello_world'])
+def hello(message):
+    bot.send_message(message.chat.id, 'hello-hello')
+
+
 @bot.message_handler(commands=['low'])
-def low(message):
-    bot.send_message(message.chat.id, messages.how_many_goods_message)
-    # получить аргумент и передать в command_func аргументом
-    bot.send_message(message.chat.id, command_func(11))
+def low_command(message):
+    bot.send_message(message.chat.id, messages.how_many_goods)
+    bot.register_next_step_handler(message, low_command_output)
+
+
+def low_command_output(message):
+    how_many = message.text
+    bot.send_message(message.chat.id, command_func(how_many=how_many))
 
 
 @bot.message_handler(commands=['high'])
-def high(message):
-    bot.send_message(message.chat.id, messages.how_many_goods_message)
-    # получить аргумент и передать в command_func
-    bot.send_message(message.chat.id, command_func(11, reverse=True))
+def high_command(message):
+    bot.send_message(message.chat.id, messages.how_many_goods)
+    bot.register_next_step_handler(message, high_command_output)
+
+
+def high_command_output(message):
+    how_many = message.text
+    bot.send_message(message.chat.id, command_func(how_many=how_many, reverse=True))
 
 
 @bot.message_handler(commands=['hot'])
-def hot(message):
-    bot.send_message(message.chat.id, messages.how_many_goods_message)
-    # получить аргумент и передать в command_func
-    bot.send_message(message.chat.id, command_func(11, reverse=True, sorted_by='orders_quantity'))
+def hot_command(message):
+    bot.send_message(message.chat.id, messages.how_many_goods)
+    bot.register_next_step_handler(message, hot_command_output)
+
+
+def hot_command_output(message):
+    how_many = message.text
+    bot.send_message(message.chat.id, command_func(how_many=how_many, reverse=True, sorted_by='orders_quantity'))
 
 
 @bot.message_handler(commands=['custom'])
-def hot(message):
-    bot.send_message(message.chat.id, messages.how_many_goods_message)
-    # получить количество товаров
-    bot.send_message(message.chat.id, messages.low_lim_message)
-    # получить нижнюю границу цен
-    bot.send_message(message.chat.id, messages.high_lim_message)
-    # получить верхнюю границу цен
+def custom_command(message):
+    bot.send_message(message.chat.id, messages.how_many_goods)
+    bot.register_next_step_handler(message, custom_how_many)
 
-    # передать все выше в аргументы command_func
-    bot.send_message(message.chat.id, command_func(11, low_lim=3, high_lim=7))
+
+def custom_how_many(message):
+    how_many = message.text
+    bot.send_message(message.chat.id, messages.low_lim_message)
+    bot.register_next_step_handler(message, custom_low_lim, how_many)
+
+
+def custom_low_lim(message, how_many):
+    low_lim = message.text
+    bot.send_message(message.chat.id, messages.high_lim_message)
+    bot.register_next_step_handler(message, custom_high_lim, how_many, low_lim)
+
+
+def custom_high_lim(message, how_many, low_lim):
+    high_lim = message.text
+    bot.send_message(message.chat.id, command_func(how_many=how_many, low_lim=low_lim, high_lim=high_lim))
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -46,9 +77,15 @@ def start_or_help_message(message):
     bot.send_message(message.chat.id, messages.help_message)
 
 
-@bot.message_handler(content_types=["text"])
-def echo_text(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(content_types=['text'])
+def hello(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет :)')
+
+
+# @bot.message_handler(content_types=["text"])
+# def echo_text(message):
+#     bot.send_message(message.chat.id, message.text)
 
 
 if __name__ == '__main__':
