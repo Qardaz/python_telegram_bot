2ef531c3b88816880779b2b99c2ffe04618ba28d
Kartyshov Dmitry <kartyshov.dmitry@yandex.ru>
2024-05-30 16:02:50 +0300

    Небольшой фикс /history

--- core.py
+++ core.py
@@ -113,7 +113,7 @@ def history_input_func(message):
 
 def history_output_func(message):
     output_message = 'История Ваших последних 10 запросов:\n'
-    for history in History.select().where(History.user_id == message.from_user.id):
+    for history in History.select().order_by(History.datetime.desc()).where(History.user_id == message.from_user.id).limit(10):
         output_message += f'{history.datetime} {history.message}\n'
 
     return output_message
